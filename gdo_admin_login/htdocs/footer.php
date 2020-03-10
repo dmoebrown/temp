a6720879cfc804c7ce26329f7bc0cb2ec8c0da94
Dustin Brown <Dabrown8@ncsu.edu>
2020-03-09 21:02:55 -0400

    h

---
+++ gdo_admin_login/htdocs/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="text-center"><a href="login.php" class="text-center">login test</a></div>
+	<div class="text-center"><a href="register.php" class="text-center">register test</a></div>
+	<div class="text-center"><a href="fetch_applicants.php" class="text-center">See Applicants test</a></div>
+</footer>
